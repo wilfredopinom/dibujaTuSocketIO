0ff937fa50e90e610ec3f0e181857ef8c56cd078
wilfredopinom <wilfredopinom@gmail.com>
2024-11-15 09:44:41 +0100

    mejora es las coordenadas, pintar con lineas, mas colores

--- client/src/main.ts
+++ client/src/main.ts
@@ -14,12 +14,22 @@ function conectarServidor(e: Event) {
   socket.on('hanDibujado', (info) => {
 
     if (!ctx) return
+   /* if(lastX ==null || lastY == null){
+      lastX =info.x
+      lastY=info.y
+      return
+    }*/
     ctx?.beginPath()
-    ctx?.arc(info.x, info.y, 4, 0, 2 * Math.PI) //2*Math.PI es igual a 360 grados
+    //ctx?.arc(info.x, info.y, 4, 0, 2 * Math.PI) //2*Math.PI es igual a 360 grados
+    ctx.moveTo(info.lastX,info.lastY)
+    ctx.lineTo(info.x,info.y)
     ctx.strokeStyle = info.c
-    ctx.fillStyle = info.c
+   // ctx.fillStyle = info.c
     ctx?.stroke()
-    ctx?.fill()
+    ctx.closePath()
+   // ctx?.fill()
+    lastX =info.x
+    lastY= info.y
   })
 
 }
@@ -31,20 +41,46 @@ canvas.height = 600
 const ctx = canvas.getContext('2d')
 document.body.appendChild(canvas)
 canvas.style.border = '1px solid black'
+let estaPulsando=false
+let lastX: 0
+let lastY: 0
+
+
+
+
+
+canvas.addEventListener('mouseup', dejaDePulsar)
+function dejaDePulsar(){
+  estaPulsando= false
+}
+
+canvas.addEventListener('mousedown', pulsa)
+function pulsa(e:MouseEvent){
+  estaPulsando= true
+const bounding =canvas.getBoundingClientRect()
+lastX = e.clientX - bounding.left
+lastY = e.clientY - bounding.top
+}
+
 
 canvas.addEventListener('mousemove', dibujar)
 
 function dibujar(e: MouseEvent) {
 
-  if (!socket) return
+  if (!socket || !estaPulsando) return
 
+  const bounding =canvas.getBoundingClientRect()
   const $color = document.getElementById('color') as HTMLSelectElement
   const info = {
-    x: e.clientX,
-    y: e.clientY,
-    c: $color.value
-  }
+  lastX: lastX,
+  lastY: lastY,
+  x: e.clientX - bounding.left,
+  y: e.clientY - bounding.top,
+  c: $color.value
+}
+
   socket.emit('dibujar', info)
+
 }
 
 
